89fe07ad3155cac497080b448db68ad8201c8fe3
Vito <vito.coquet@gmail.com>
2022-02-25 14:18:38 +0100

    pushing some files

---
+++ GridGenerator.py
@@ -0,0 +1,9 @@
+
+
+def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row)
+        print(row)
